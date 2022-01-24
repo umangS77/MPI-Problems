070e5b67ae14c8a6512be69911ade203b2866b34
Umang Srivastava <umang.srivastava@students.iiit.ac.in>
2022-01-24 21:47:20 +0530

    Q1. <11 cases left and general input/output left

---
+++ Q1.cpp
@@ -0,0 +1,117 @@
+#include <iostream>
+#include <mpi.h>
+#include <math.h>
+using namespace std;
+
+
+
+int checkPrime(int lowerLimit, int upperLimit, int num)
+{
+    if(num == 2)
+        return 0;
+
+    if(num%2==0)
+        return 1;
+
+    for(int i=lowerLimit;i<=upperLimit;i+=2)
+    {
+        if(num%i==0)
+            return 1;
+    }
+
+    return 0;
+}
+
+int main( int argc, char **argv ) {
+    int rank, numprocs;
+
+    // initiate MPI
+    MPI_Init( &argc, &argv );
+
+    // get size of the current communicator
+    MPI_Comm_size( MPI_COMM_WORLD, &numprocs );
+
+    // get current process rank
+    MPI_Comm_rank( MPI_COMM_WORLD, &rank );
+
+    /*synchronize all processes*/
+    MPI_Barrier( MPI_COMM_WORLD );
+    // double start_time = MPI_Wtime();
+    // int primelist [] = {3,5,7,11,13,17,19,}
+    int root_rank = 0;
+    int n = 1000000000;
+    int sqRoot = sqrt(n);
+    int lim = numprocs; // 10 = 11 - 1
+    int val = sqRoot/lim; // number of values for 1 thread
+    int answer = 0;
+    
+    if(val == 0)
+    {
+
+    }
+
+    else
+    {
+        int reduction_result = 0;
+
+        int lowerLimit = val*rank;
+        if(rank == 0)
+            lowerLimit = 3;
+        int upperLimit = lowerLimit + val-1;
+        if(rank == lim-1 || upperLimit > sqRoot)
+            upperLimit = sqRoot;
+
+        int f=0;
+        if(lowerLimit <= upperLimit)
+        f = checkPrime(lowerLimit, upperLimit, n);
+
+        printf("rank = %d ------  LL = %d,  UL = %d,   f = %d\n", rank, lowerLimit, upperLimit, f);
+
+        MPI_Reduce(&f, &reduction_result, 1, MPI_INT, MPI_SUM, root_rank, MPI_COMM_WORLD);
+
+
+        if(rank == root_rank)
+        {
+            printf("result = %d\n", reduction_result);
+            if(reduction_result > 0)
+                printf("NO\n");
+            else
+                printf("YES\n");
+        }
+    }
+
+    
+
+
+
+
+    
+    
+
+
+
+
+
+        
+
+    
+
+
+
+    
+
+    // enter your code here
+
+    MPI_Barrier( MPI_COMM_WORLD );
+    // double end_time = MPI_Wtime() - start_time;
+    // double maxTime;
+    // // get max program run time for all processes
+    // MPI_Reduce( &end_time, &maxTime, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD );
+    // if ( rank == 0 ) {
+    //     cout<<"Total time (s): "<<maxTime<<"\n";
+    // }
+
+    // shut down MPI and close
+    MPI_Finalize();
+    return 0;
+}
